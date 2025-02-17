c739cdf2d4469d44e99a26e0bf14b8699873eece
= <=>
2025-02-17 14:32:54 -0300

    Public

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import morgan from 'morgan'
-
+import path from 'path' // Aquí se importa el módulo path
 
 const app = express()
 
 app.use(morgan('dev'))
 
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(process.cwd(), 'public')))
+
+// Ruta principal que renderiza index.html
 app.get('/', (req, res) => {
-  res.send('<h1>Esto es el chat</h1>')
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'))
 })
 
 app.listen(3000, () => {
   console.log('Server running port http://localhost:3000')
 })
-
