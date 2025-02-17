f5c7872cab20a158066349501a2f388b6fd34579
= <=>
2025-02-17 13:03:09 -0300

    Morgan

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import morgan from 'morgan'
+
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Esto es el chat</h1>')
