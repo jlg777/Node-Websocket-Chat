9803aacc5fe3b3a8009cafe70db0fa8c99a2c81d
= <=>
2025-02-21 15:55:38 -0300

    update

--- src/server/index.js
+++ src/server/index.js
@@ -11,29 +11,60 @@ const app = express()
 
 app.use(morgan('dev'))
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
 
 import { createClient } from "@libsql/client";
 
-export const turso = createClient({
+export const db = createClient({
   url: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
-await turso.execute(`
+await db.execute(`
   CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   content TEXT
   )
   `)
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('a user conected')
   io.emit('chat message', 'usuario conectado')
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+       result = await db.execute({
+        sql: `INSERT INTO messages (content) VALUES (:msg)`,
+        args: {msg}
+      })
+    } catch (error) {
+      console.error(error)
+    }
+    if (result && result.lastInsertRowid) {
+      console.log('ID de mensaje insertado:', result.lastInsertRowid.toString());  // Verifica el valor
+      io.emit('chat message', msg, result.lastInsertRowid.toString());  // Emite el mensaje con el ID
+    } else {
+      console.error('Inserción fallida o lastInsertRowid no encontrado');
+    }
   })
+
+  //console.log(result.lastInsertRowid.toString())
+  if(!socket.recovered) {
+    try {
+      const result = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+      result.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
